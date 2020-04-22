9d8c7cafae7a3e4b7ad34dd36d9f851f249403c0
Daniel Hegner <daniel.hegner@sveaekonomi.se>
2020-04-22 13:50:09 +0200

    Basic files to expose problem with alternate nuget cache location

---
+++ setup-alternateCacheLocations.ps1
@@ -0,0 +1,15 @@
+$cacheBasePath = "C:\_cache";
+# Set the nuget cache path
+$nugetCachePath = "$cacheBasePath\_nuget"
+New-Item -Path $nugetCachePath -ItemType "directory" -ErrorAction SilentlyContinue
+[Environment]::SetEnvironmentVariable("NUGET_PACKAGES", $nugetCachePath, "Machine")
+
+# Set the paket cache path
+$paketCachePath = "$cacheBasePath\_paket"
+New-Item -Path $paketCachePath -ItemType "directory" -ErrorAction SilentlyContinue
+[Environment]::SetEnvironmentVariable("NuGetCachePath", $paketCachePath, "Machine")
+
+# Set nuget http cache path
+$nugetHttpCachePath = "$cacheBasePath\_nugetHttp"
+New-Item -Path $nugetHttpCachePath -ItemType "directory" -ErrorAction SilentlyContinue
+[Environment]::SetEnvironmentVariable("NUGET_HTTP_CACHE_PATH", $nugetHttpCachePath, "Machine")
