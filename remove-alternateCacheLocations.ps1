8246c93987485f5b987a3a097416c0e9fea56c55
Daniel Hegner <daniel.hegner@sveaekonomi.se>
2020-04-22 13:52:30 +0200

    cleaned up removal

--- remove-alternateCacheLocations.ps1
+++ remove-alternateCacheLocations.ps1
@@ -1,15 +1,6 @@
-$cacheBasePath = "C:\_cache";
-# Set the nuget cache path
-$nugetCachePath = "$cacheBasePath\_nuget"
-New-Item -Path $nugetCachePath -ItemType "directory" -ErrorAction SilentlyContinue
+# Remove the nuget cache path
 [Environment]::SetEnvironmentVariable("NUGET_PACKAGES", $null, "Machine")
-
-# Set the paket cache path
-$paketCachePath = "$cacheBasePath\_paket"
-New-Item -Path $paketCachePath -ItemType "directory" -ErrorAction SilentlyContinue
+# Remove the paket cache path
 [Environment]::SetEnvironmentVariable("NuGetCachePath", $null, "Machine")
-
-# Set nuget http cache path
-$nugetHttpCachePath = "$cacheBasePath\_nugetHttp"
-New-Item -Path $nugetHttpCachePath -ItemType "directory" -ErrorAction SilentlyContinue
+# Remove nuget http cache path
 [Environment]::SetEnvironmentVariable("NUGET_HTTP_CACHE_PATH", $null, "Machine")
