d3411729fe70b5d73ce4a698768effebfdac6251
Daniel Hegner <daniel.hegner@sveaekonomi.se>
2020-04-22 15:37:21 +0200

    restored setup script

--- setup-alternateCacheLocations.ps1
+++ setup-alternateCacheLocations.ps1
@@ -5,12 +5,12 @@ $nugetCachePath = "$cacheBasePath\nuget"
 New-Item -Path $nugetCachePath -ItemType "directory" -ErrorAction SilentlyContinue
 [Environment]::SetEnvironmentVariable("NUGET_PACKAGES", $nugetCachePath, "Machine")
 
-# Set the paket cache path
+# #Set the paket cache path
 # $paketCachePath = "$cacheBasePath\_paket"
 # New-Item -Path $paketCachePath -ItemType "directory" -ErrorAction SilentlyContinue
 # [Environment]::SetEnvironmentVariable("NuGetCachePath", $paketCachePath, "Machine")
 
-# Set nuget http cache path
+# #Set nuget http cache path
 # $nugetHttpCachePath = "$cacheBasePath\_nugetHttp"
 # New-Item -Path $nugetHttpCachePath -ItemType "directory" -ErrorAction SilentlyContinue
 # [Environment]::SetEnvironmentVariable("NUGET_HTTP_CACHE_PATH", $nugetHttpCachePath, "Machine")
